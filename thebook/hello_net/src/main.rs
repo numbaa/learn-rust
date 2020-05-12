0f904ec2188e9945fdc638b3b2537746baf09ae2
Zhennan Tu <zhennan.tu@gmail.com>
2020-05-12 11:12:57 +0800

    single thread

---
+++ thebook/hello_net/src/main.rs
@@ -0,0 +1,29 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs;
+use std::io::prelude::*;
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:7676").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "simple.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, content);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
